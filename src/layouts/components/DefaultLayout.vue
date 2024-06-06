0ccb874d11a8b22e63e5a432b1d3e8414d2209c3
Andrew <30905073+0ndrec@users.noreply.github.com>
2024-06-06 13:17:45 +0300

    _

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -78,7 +78,7 @@ dayjs()
       class="w-64 fixed z-50 left-0 top-0 bottom-0 overflow-auto bg-base-100 border-r border-gray-100 dark:border-gray-700"
       :class="{ block: sidebarShow, 'hidden xl:!block': !sidebarShow }"
     >
-      <div class="flex justify-between mt-1 py-4 mb-1" style="justify-content: center;>
+      <div class="flex justify-between mt-1 py-4 mb-1" style="justify-content: center;">
         <RouterLink to="/" class="flex items-center">
           <img style="padding-inline: 20px; src="../../assets/logo.svg" />
   
